d8d681c1262ad3f09070b80bee82372d166ad377
bgaszczyk <bloo292@gmail.com>
2024-07-22 11:27:11 +0200

    dynamic orgs on index page

--- DonationCore/views.py
+++ DonationCore/views.py
@@ -2,15 +2,17 @@ from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
 
-from DonationCore.models import Donation, Institution
+from DonationCore.models import Donation, Institution, Category
 
 
 class LandingPageView(View):
     def get(self, request):
+        institutions = Institution.objects.prefetch_related('categories')
         total_quantity = Donation.objects.aggregate(total=Sum('quantity'))['total']
         institutions_number = Institution.objects.count()
         return render(request, 'index.html', {'total_quantity': total_quantity,
-                                              'institutions_number': institutions_number})
+                                              'institutions_number': institutions_number,
+                                              'institutions': institutions})
 
 
 class RegisterPageView(View):
